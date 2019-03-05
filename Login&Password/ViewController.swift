c71b4e6a190232f1b62a0bd1f2e8cf0ee299e36d
macbook pro 665 <macbookpro665@macbook-mac133-2.local>
2019-03-05 15:17:51 +0300

    Initial Commit

---
+++ Login&Password/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login&Password
+//
+//  Created by macbook pro 665 on 05/03/2019.
+//  Copyright © 2019 Pavel Meshcheryakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
